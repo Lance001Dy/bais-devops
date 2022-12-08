9d38c9d02995273f69abe41f79e1dba2935f10d8
Lance Dyer <ladyer098@gmail.com>
2022-12-08 08:21:32 +0200

    my first commit

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function About() {
+  return (
+    <Layout>
+    <div>           
+      <h1> About </h1>
+      <p>Domain names and Email Hosting</p>
+      <p>Web Hosting</p>
+    </div>
+    </Layout>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import * as React from "react"
+import Layout from "../components/Layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+  <Layout>
+  <section>
+  <div>
+    <h1>Brainwave Advertising & I.T Solutions</h1>
+    <h4> For all your Advertising and Marketing needs as well as Web and Social Development.</h4>
+    <h6> Get your business on the map,with our team of engineers that can provide you from domain names and email Hosting
+         too Website Development and Social Platform Marketing.</h6>
+  </div>
+  </section>
+  </Layout>
+  )
 }
